5d8ec9f2fa3e55164a02bf284bba84ce88815e56
touchGuy <49477017+Ray569743422@users.noreply.github.com>
2023-08-25 13:19:14 +0800

    Add files via upload

---
+++ 06.spearman_correlation/01.correlations.sh
@@ -0,0 +1,6 @@
+/dellfsqd2/ST_OCEAN/USER/wangrui21/03.software/anaconda3_5_3_0/envs/SAMap/bin/python 01.correlations.py\
+	-genes 00.Danio_rerio_TF_121.txt\
+	-early 01.earlyTF121.pdf\
+	-later 01.laterTF121.pdf\
+	-zfDot 01.zf_TF_121.pdf\
+	-scDot 01.sc_TF_121.pdf
